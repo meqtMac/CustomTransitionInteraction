b12a8db026688f2d7f19866c1f1e2cbadc536f9c
meqtjiang <meqtjiang@tencent.com>
2025-08-19 19:56:37 +0800

    Initial Commit

---
+++ CustomTransitionInteraction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTransitionInteraction
+//
+//  Created by jiangYi on 2025/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
